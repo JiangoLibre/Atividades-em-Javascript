bae8fcfac3a638ce6c16e0114f7e4f6e21fa5d48
João Eduardo <49443445+JiangoLibre@users.noreply.github.com>
2023-04-18 21:37:06 -0300

    Update index.js
    Mudança no nome das variáveis para a leitura do código ser mais simples

--- sequencia_fibonacci/index.js
+++ sequencia_fibonacci/index.js
@@ -1,21 +1,21 @@
 // Define o número a ser verificado
-const num = 13;
+const numeroVerificado = 13;
 
 // Define as variáveis para calcular a sequência
-let prev = 0;
-let curr = 1;
-let next = 0;
+let primeiroNumero = 0;
+let segundoNumero = 1;
+let proximoNumero = 0;
 
 // Loop para calcular a sequência até o próximo valor ser maior ou igual ao número informado
-while (next < num) {
-  next = prev + curr;
-  prev = curr;
-  curr = next;
+while (proximoNumero < numeroVerificado) {
+  proximoNumero = primeiroNumero + segundoNumero;
+  primeiroNumero = segundoNumero;
+  segundoNumero = proximoNumero;
 }
 
 // Verifica se o número informado pertence à sequência
-if (next === num) {
-  console.log(`${num} pertence à sequência de Fibonacci`);
+if (proximoNumero === numeroVerificado) {
+  console.log(`${numeroVerificado} pertence à sequência de Fibonacci`);
 } else {
-  console.log(`${num} não pertence à sequência de Fibonacci`);
+  console.log(`${numeroVerificado} não pertence à sequência de Fibonacci`);
 }
